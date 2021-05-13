13ff064aef05e44574ecf5e931ad42bce2a5c962
Sardorkhuja <mr.khuzha@gmail.com>
2021-05-13 10:35:49 +0300

    adding wordpress

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,3 +1,16 @@
 apt install -y mariadb-server;
 service mysql start;
-bash;
+service mysql status;
+
+echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'${DB_HOST}' IDENTIFIED BY '${DB_PASSWORD}';";
+echo "CREATE USER '${DB_USER}'@'${DB_HOST}' IDENTIFIED BY '${DB_PASSWORD}';" | mariadb;
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mariadb;
+echo "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'${DB_HOST}';";
+echo "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'${DB_HOST}';" | mariadb;
+echo "update mysql.user set plugin='mysql_native_password' where user='${DB_USER}';";
+echo "update mysql.user set plugin='mysql_native_password' where user='${DB_USER}';" | mariadb;
+echo "FLUSH PRIVILEGES;";
+echo "FLUSH PRIVILEGES;" | mariadb;
+
+bash
