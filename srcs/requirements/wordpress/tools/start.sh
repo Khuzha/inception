a685fed81020097166f5dd95af76d5a389d4126c
Sardorkhuja <mr.khuzha@gmail.com>
2021-07-20 21:19:04 +0300

    fix wordpress

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -4,6 +4,7 @@ sed -i -e "s/\${DB_USER}/${DB_USER}/g" /tmp/wp-config.php;
 sed -i -e "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /tmp/wp-config.php;
 
 mv /tmp/wp-config.php /var/www/html/wordpress;
+mv /tmp/fpm.conf /etc/php/7.3/fpm/pool.d/www.conf;
 
 chmod -R 775 /var/www/html/wordpress;
 chown -R www-data /var/www/html/wordpress;
@@ -11,4 +12,5 @@ chgrp -R www-data /var/www/html/wordpress;
 
 # php -S 0.0.0.0:9000 -t /var/www/html/wordpress;
 
+# tail -f /dev/null;
 /usr/sbin/php-fpm7.3 -F
